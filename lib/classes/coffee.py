ec052f8f18672c6d7c5e33359387e0c3b7268d9c
Cecilia <ccchimento@gmail.com>
2024-02-06 13:42:31 -0700

    completed lab

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,28 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if type(new_name) == str and not hasattr(self, "name"):
+            self._name = new_name
+        else:
+            raise Exception
         
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        return [order for order in Order.all if order.coffee == self]
     
-    def customers(self, new_customer=None):
-        from classes.customer import Customer
-        pass
+    def customers(self): #new_customer=None):
+        # from classes.customer import Customer
+        return list(set([order.customer for order in self.orders()]))
     
     def num_orders(self):
-        pass
+        return len(self.orders())
     
     def average_price(self):
-        pass
+        return sum([order.price for order in self.orders()]) / len(self.orders())
--- lib/debug.py
+++ lib/debug.py
@@ -8,4 +8,17 @@ from classes.coffee import Coffee
 if __name__ == '__main__':
     print("HELLO! :) let's debug")
 
+    coffee = Coffee("Hazelnut Latte")
+    coffee_2 = Coffee("Mocha")
+    
+    customer = Customer("Steve")
+    customer_2 = Customer("Bob")
+    
+    order_1 = Order(customer, coffee, 2)
+    order_2 = Order(customer, coffee, 5)
+    order_3 = Order(customer, coffee_2, 5)
+    order_4 = Order(customer_2, coffee, 2)
+    order_5 = Order(customer_2, coffee_2, 3)
+    order_6 = Order(customer_2, coffee_2, 5)
+
     ipdb.set_trace()
